2588c10c27e55c693f7614db724f7f73b8e6e2dc
Lukasz Szczepanek <lukas.szczepanek@gmail.com>
2017-01-21 15:01:43 +0100

    Adding first method sygnature and before tag

--- src/test/java/lab3_1/TestBookKeeper.java
+++ src/test/java/lab3_1/TestBookKeeper.java
@@ -1,5 +1,23 @@
 package lab3_1;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sales.domain.invoicing.BookKeeper;
+
 public class TestBookKeeper {
 
+	BookKeeper bookKeeper;
+	
+	
+	@Before
+	public static void before(){
+		
+	}
+	
+	
+	@Test
+	public static void testOneInvoiceRequest(){
+		
+	}
 }
