fb476f6617c047706093a686e94889c20307ad1a
Lukasz Szczepanek <lukas.szczepanek@gmail.com>
2017-01-21 16:59:45 +0100

    Time to try second option with behaviour test, but I am not fully sure I can make it at least nice, still not sure my previous test is working fine

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TestBookKeeperWithItemsAndThereShuoldBeTwoTimesCalucatedTaxes.java
@@ -0,0 +1,26 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+
+
+public class TestBookKeeperWithItemsAndThereShuoldBeTwoTimesCalucatedTaxes {
+
+}
