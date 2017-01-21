1b6bc9ffee834be9be8c772c875171e979b396ba
Lukasz Szczepanek <lukas.szczepanek@gmail.com>
2017-01-21 16:50:31 +0100

    With google help I made this working somehow, I understand whats going on a little bit after 2,5h of searching/trying things, but still I am not sure my test failing because is bad or because he is really working fine #BLOP

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TestBookKeeper.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TestBookKeeper.java
@@ -3,6 +3,7 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -11,6 +12,12 @@ import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 
 public class TestBookKeeper {
@@ -27,20 +34,35 @@ public class TestBookKeeper {
     Invoice invoice;
     @Mock
     TaxPolicy taxPolicy;
+    @Mock
+    ProductData productData;
+    @Mock
+    Money money;
+    @Mock
+    Tax tax;
 	
 	
 	
 	@Before
 	public void before(){
-        invFactory = mock(InvoiceFactory.class);
+        invFactory = new InvoiceFactory();
         bookKeeper = new BookKeeper(invFactory);
         invRequest = mock(InvoiceRequest.class);
         requestItem = mock(RequestItem.class);
+        taxPolicy = mock(TaxPolicy.class);
+        productData = mock(ProductData.class);
+        money = mock(Money.class);
+        tax = mock(Tax.class);
+        invoice = bookKeeper.issuance(invRequest, taxPolicy);
         ArrayList<RequestItem> requestItems = new ArrayList<>();
         requestItems.add(requestItem);
         when(invRequest.getItems()).thenReturn(requestItems);
-        taxPolicy = mock(TaxPolicy.class);
-        invoice = bookKeeper.issuance(invRequest, taxPolicy);
+        when(invRequest.getClientData()).thenReturn(new ClientData(new Id(""),""));
+        when(requestItem.getProductData()).thenReturn(productData);
+        when(requestItem.getTotalCost()).thenReturn(Money.ZERO);
+        when(tax.getAmount()).thenReturn(Money.ZERO);
+        when(taxPolicy.calculateTax(any(ProductType.class),any(Money.class))).thenReturn(tax);    
+
 	}
 	
     @Test
